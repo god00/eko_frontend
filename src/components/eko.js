a59c82e87a85c0b34a01316e2b76412b0dbb0848
Phosawat <phosawat.o@gmail.com>
2020-01-27 23:42:56 +0700

    Add eko.js

---
+++ src/components/eko.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Eko extends Component {
+    render() {
+      return (
+          <div>
+            <h2>Eko</h2>
+          </div>
+      );
+    }
+  }
+  
+  export default Eko;
